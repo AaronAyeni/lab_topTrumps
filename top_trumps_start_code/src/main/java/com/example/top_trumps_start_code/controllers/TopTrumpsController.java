ddc771cd2cee34e7b3176d8b82a8b0dd3d1fb74f
Aaron Ayeni <aaronayeni135@gmail.com>
2024-05-30 16:40:12 +0100

    completed mvp point 1

---
+++ top_trumps_start_code/src/main/java/com/example/top_trumps_start_code/controllers/TopTrumpsController.java
@@ -0,0 +1,30 @@
+package com.example.top_trumps_start_code.controllers;
+
+import com.example.top_trumps_start_code.models.Card;
+import com.example.top_trumps_start_code.services.TopTrumpsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+
+@RestController
+@RequestMapping("/toptrumps")
+public class TopTrumpsController {
+
+    @Autowired
+    TopTrumpsService topTrumpsService;
+
+    @PostMapping("/compare")
+   public ResponseEntity<String> checkWinner(@RequestBody List<Card> cards){
+        String message = topTrumpsService.compareCards(cards);
+        return new ResponseEntity<>(message, HttpStatus.OK);
+
+    }
+}
---
+++ top_trumps_start_code/src/main/java/com/example/top_trumps_start_code/services/TopTrumpsService.java
@@ -0,0 +1,33 @@
+package com.example.top_trumps_start_code.services;
+
+import com.example.top_trumps_start_code.models.Card;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+@Service
+public class TopTrumpsService {
+
+    public String compareCards(List<Card> cards) {
+        if (cards == null || cards.size() != 2) {
+            return "Invalid input. Please provide exactly two cards.";
+        }
+
+        Card card1 = cards.get(0);
+        Card card2 = cards.get(1);
+
+        int card1Value = card1.getCardValue();
+        int card2Value = card2.getCardValue();
+
+        if (card1Value > card2Value) {
+            return card1.getRank() + "of" + card1.getSuit() + "wins!";
+        } else if (card1Value < card2Value) {
+            return card2.getRank() + " of " + card2.getSuit() + " wins!";
+        } else {
+            return "It's a tie!";
+        }
+    }
+
+
+}
